2a22627196e4f966996349d6498540628f7cd014
Feliciotti <feliciotti@hotmail.com>
2022-03-22 02:21:21 -0300

    solo la primer parte

--- server/ej.js
+++ server/ej.js
@@ -1,41 +1,22 @@
-// Clase contenedor a ser importada
+// Clase productos a ser exportada
 
-class Contenedor {
-    constructor(products){
-        this.products = products
-    };
-
-    getAll() {
-        return this.products
-    };
-
-};
-
-//Ejemplo
-
-class User {
-    constructor(name, age, datos) {
-      this.name = name;
-      this.age = age;
-      this.datos = datos;
+class productos {
+    constructor(productos) {
+      this.productos = productos;
     }
   
-    getUserStats() {
-        const DatosParsed = JSON.parse(JSON.stringify(this.datos));
-        
-        return DatosParsed
-//         const productsParsed = JSON.parse(products);        
-//         const productById = productsParsed.find(function (product) {
-
-//             fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2))
-
+    getAll() {
+        const productosParsed = JSON.parse(JSON.stringify(this.productos));     
+        return productosParsed
+    }
+    
+    getRandom() {
+        const productosParsed = JSON.parse(JSON.stringify(this.productos));
+        // TODO: agregar o definir una funciona que itere por todos los elemntos y devuelva la cantidad
+        // y usar esa cantidad con algo tipo math.random para que use un número random
+        // Lo intenté y creo que me acercé pero solo lo veía en la consola, parece que a express no le gustaba
     }
   }
-  
-console.log("Hola Modd")
-
-const mytest = "variable de prueba"
 
 // Exportamos el modulo
-module.exports = Contenedor
-module.exports = User
+module.exports = productos
--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@
 const express = require('express');
 const app = express();
 
-// Importar Clase Contenedor
-const Contenedor = require('./ej.js');
-const p = new Contenedor('sdksdfsd');
-
 // Datos Servidor Express
 const PORT = 8080
 const server = app.listen(PORT, () => {
@@ -13,29 +9,29 @@ const server = app.listen(PORT, () => {
 });
 server.on("error", error => console.log(`Error en el servidor ${error}`));
 
+// Constante productos que respeta el nombre con el que fue exportada, de tener otro nombre no funciona
+const productos = require('./ej.js');
 
-// objecto
-// {producto:'tabla',precio:250} x3
-
-// ejemplo array
-// [{"title": "producto2","price": 500,"id": 1},{"title": "producto3","price": 600,"id": 2},{"title": "producto4","price": 1000,"id": 3}]
-
-const User = require('./ej.js');
-const jim = new User('Jim', 37, [{"title": "producto2","price": 500,"id": 1},{"title": "producto3","price": 600,"id": 2},{"title": "producto4","price": 1000,"id": 3}]);
-
-console.log(jim.getUserStats());
+//Objecto creado en base a la class constructor productos que importamos
+const lista = new productos([{"title": "producto2","price": 500,"id": 1},{"title": "producto3","price": 600,"id": 2},{"title": "producto4","price": 1000,"id": 3}]);
 
 // Metodos Express
 app.get('/products', (req, res) => {
-    res.send(jim.getUserStats())
+    res.send(lista.getAll())
 });
 
 app.get('/random', (req, res) =>{
-    res.send(jim.getUserStats())
+    res.send(lista.getRandom())
 });
+
+console.log(lista.getAll());
+console.log(lista.getRandom());
+
 
 // ref exports
 // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 
 // DATAZO LEER
 // https://nkaushik.com/javascript/javascript-syntaxerror-unexpected-token-json/
+
+// Tip: no meter 20 cambios en un saque al debuggear, sacar 1-2 cosas, probar e ir tanteando como se comportar el código y continuar
